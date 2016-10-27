111e40e524d97eb58bbe3ed2679b7f5d1aeed1fd
Brandon Bielicki <Brandonbielicki@gmail.com>
2016-10-27 23:52:34 +0000

    su command for debian, mkgrp adds packages

--- scripts/ppa_setup.sh
+++ scripts/ppa_setup.sh
@@ -14,8 +14,20 @@ sudo add-apt-repository -y ppa:mystic-mirage/komodo-edit
 VER=$(cat /etc/debian_version)
 ID="${VER%/*}"
 echo "  distro name: $ID" >> $LOGFILE
-echo "deb http://deb.torproject.org/torproject.org $ID main" | sudo tee -a /etc/apt/sources.list
-echo "deb-src http://deb.torproject.org/torproject.org $ID main" | sudo tee -a /etc/apt/sources.list
+
+if grep -Fxq "deb http://deb.torproject.org/torproject.org $ID main" /etc/apt/sources.list; then
+    echo "  tor already in sources" >> $LOGFILE
+else
+    echo "  adding tor to sources" >> $LOGFILE
+    echo "deb http://deb.torproject.org/torproject.org $ID main" | sudo tee -a /etc/apt/sources.list
+fi
+if grep -Fxq "deb-src http://deb.torproject.org/torproject.org $ID main" /etc/apt/sources.list; then
+    echo "  tor already in sources" >> $LOGFILE
+else
+    echo "  adding tor to sources" >> $LOGFILE
+    echo "deb-src http://deb.torproject.org/torproject.org $ID main" | sudo tee -a /etc/apt/sources.list
+fi
+
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
--- scripts/settings.sh
+++ scripts/settings.sh
@@ -9,12 +9,12 @@ HOMEDIR="$(eval echo ~$USER)"
 echo "  Running as user $USER" >> $LOGFILE
 echo "  $USER's home directory: $HOMEDIR" >> $LOGFILE
 
-su $USER --session-command "xfconf-query -c pointers -p /ETPS2_Elantech_Touchpad/Properties/Synaptics_Tap_Action -n -t int -t int -t int -t int -t int -t int -t int  -s 0 -s 0 -s 0 -s 0 -s 1 -s 3 -s 2"
-su $USER --session-command "xfconf-query -c pointers -p /ETPS2_Elantech_Touchpad/Properties/libinput_Tapping_Enabled -n -t int -s 1"
+su $USER -c "xfconf-query -c pointers -p /ETPS2_Elantech_Touchpad/Properties/Synaptics_Tap_Action -n -t int -t int -t int -t int -t int -t int -t int  -s 0 -s 0 -s 0 -s 0 -s 1 -s 3 -s 2"
+su $USER -c "xfconf-query -c pointers -p /ETPS2_Elantech_Touchpad/Properties/libinput_Tapping_Enabled -n -t int -s 1"
 
-su $USER --session-command "xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/Super_L' -n -t string -s 'xfce4-terminal --drop-down'"
-su $USER --session-command "xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/<Primary><Alt>Super_L' -n -t string -s 'xfce4-terminal'"
-su $USER --session-command "xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/<Primary><Shift>Super_L' -n -t string -s 'chromium'"
+su $USER -c "xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/Super_L' -n -t string -s 'xfce4-terminal --drop-down'"
+su $USER -c "xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/<Primary><Alt>Super_L' -n -t string -s 'xfce4-terminal'"
+su $USER -c "xfconf-query -c xfce4-keyboard-shortcuts -p '/commands/custom/<Primary><Shift>Super_L' -n -t string -s 'chromium'"
 
 sudo systemctl set-default graphical.target
 sudo systemctl enable NetworkManager.service
